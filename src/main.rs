9405bf0ce77defb8f7e6bd61c7bada0c9e2a15ce
Corey Farwell <coreyf@rwell.org>
2016-08-13 16:57:54 -0400

    Create a map instead of a vector

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rusoto;
 use rusoto::logs::{CloudWatchLogsClient, DescribeLogGroupsRequest};
 use rusoto::{DefaultCredentialsProvider, Region};
 use rusoto::logs::{LogGroupName, DescribeLogStreamsRequest, LogGroup, GetLogEventsRequest};
-use std::collection::HashMap;
+use std::collections::HashMap;
 
 fn aws_lambda_log_group_names() -> Vec<LogGroupName> {
     let mut log_groups: Vec<LogGroupName> = vec![];
@@ -12,14 +12,14 @@ fn aws_lambda_log_group_names() -> Vec<LogGroupName> {
     let client = CloudWatchLogsClient::new(credentials, Region::UsEast1);
     let mut request = DescribeLogGroupsRequest::default();
 
+    let mut map = HashMap::new();
+
     loop {
         let response = client.describe_log_groups(&request).unwrap();
         let a: Vec<LogGroup> = response.log_groups.unwrap();
-        let mut b: Vec<LogGroupName> = vec![];
         for i in a {
-            b.push(i.log_group_name.unwrap());
+            map.insert(i.log_group_name.unwrap(), i.stored_bytes.unwrap());
         }
-        log_groups.extend(b);
 
         if response.next_token.is_some() {
             request.next_token = response.next_token;
@@ -28,6 +28,7 @@ fn aws_lambda_log_group_names() -> Vec<LogGroupName> {
         }
     }
 
+    /*
     for log_group_name in &log_groups {
         let request = DescribeLogStreamsRequest {
             log_group_name: log_group_name.clone(),
@@ -40,6 +41,7 @@ fn aws_lambda_log_group_names() -> Vec<LogGroupName> {
             println!("{}", i.log_stream_name.unwrap());
         }
     }
+    */
     
     log_groups
 }
