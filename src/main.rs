ba42f1b28696d8acd9d4e87ef44161087319bf4e
Corey Farwell <coreyf@rwell.org>
2016-08-16 22:06:29 -0400

    Move function based map-generator into struct pattern.

--- src/main.rs
+++ src/main.rs
@@ -7,49 +7,54 @@ use std::collections::HashMap;
 use std::thread::sleep;
 use std::time::Duration;
 
-fn aws_lambda_log_group_map() -> HashMap<String, Option<LogStream>> {
-    let mut log_groups: Vec<LogGroupName> = vec![];
+struct LogGroupWatcher {
+    map: HashMap<String, Option<LogStream>>,
+}
 
-    let credentials = DefaultCredentialsProvider::new().unwrap();
-    let client = CloudWatchLogsClient::new(credentials, Region::UsEast1);
-    let mut request = DescribeLogGroupsRequest::default();
+impl LogGroupWatcher {
+    fn new() -> LogGroupWatcher {
+        let mut log_groups: Vec<LogGroupName> = vec![];
 
-    let mut map = HashMap::new();
+        let credentials = DefaultCredentialsProvider::new().unwrap();
+        let client = CloudWatchLogsClient::new(credentials, Region::UsEast1);
+        let mut request = DescribeLogGroupsRequest::default();
 
-    loop {
-        let response = client.describe_log_groups(&request).unwrap();
-        let a: Vec<LogGroup> = response.log_groups.unwrap();
-        for i in a {
-            let log_group_name = i.log_group_name.unwrap();
-            let request = DescribeLogStreamsRequest {
-                log_group_name: log_group_name.clone(),
-                descending: Some(true),
-                limit: Some(1),
-                order_by: Some("LastEventTime".into()),
-                ..Default::default()
-            };
-            let response = client.describe_log_streams(&request).unwrap();
-            let alpha = response.log_streams.unwrap().into_iter().next();
-            map.insert(log_group_name.clone(), alpha);
+        let mut map = HashMap::new();
 
-            // Slow down requests
-            // https://github.com/rusoto/rusoto/issues/234
-            sleep(Duration::from_millis(100));
+        loop {
+            let response = client.describe_log_groups(&request).unwrap();
+            let a: Vec<LogGroup> = response.log_groups.unwrap();
+            for i in a {
+                let log_group_name = i.log_group_name.unwrap();
+                let request = DescribeLogStreamsRequest {
+                    log_group_name: log_group_name.clone(),
+                    descending: Some(true),
+                    limit: Some(1),
+                    order_by: Some("LastEventTime".into()),
+                    ..Default::default()
+                };
+                let response = client.describe_log_streams(&request).unwrap();
+                let alpha = response.log_streams.unwrap().into_iter().next();
+                map.insert(log_group_name.clone(), alpha);
+
+                // Slow down requests
+                // https://github.com/rusoto/rusoto/issues/234
+                sleep(Duration::from_millis(100));
+            }
+
+            if response.next_token.is_some() {
+                request.next_token = response.next_token;
+            } else {
+                break;
+            }
         }
 
-        if response.next_token.is_some() {
-            request.next_token = response.next_token;
-        } else {
-            break;
+        LogGroupWatcher {
+            map: map,
         }
     }
-
-    map
 }
 
 fn main() {
-    let log_groups = aws_lambda_log_group_names();
-    for log_group in log_groups {
-        //println!("{}", log_group);
-    }
+    let log_group_watcher = LogGroupWatcher::new();
 }
